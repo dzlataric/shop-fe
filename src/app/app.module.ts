00474f1bc2214345883d11439a8c40b8e48e2fd4
dzlataric <dejanzlataric@outlook.com>
2017-08-20 17:41:10 +0200

    Added header, footer and category component placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
